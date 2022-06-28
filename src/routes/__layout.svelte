e0b11aaedc3b52ef66ccbbbd73073e10654a9455
Dan Lubbers <lubbers.dan@gmail.com>
2022-06-28 16:26:13 -0400

    Authentication is setup and functioning properly.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,31 @@
 <script>
-	import { supabase } from '../supabase.js';
 	import '../app.css';
+	import { supabase } from '../supabase.js';
+	import { user } from '../store/authStore';
+	import Navbar from '../components/Navbar.svelte';
+	import Auth from '../components/Auth.svelte';
+	import { loadTodos } from '../store/todoStore.js';
+
+	console.log('supabase.auth.user()', supabase.auth.user());
+
+	user.set(supabase.auth.user());
+
+	supabase.auth.onAuthStateChange((_, session) => {
+		user.set(session?.user);
+		if (sessions?.user) {
+			loadTodos();
+		}
+	});
 </script>
 
 <!-- The Container -->
 <!-- A wrapper for all of the children in the app -->
 <div class="container mx-auto my-6 max-w-lg px-5">
-	<!-- slot is sveltes version of a child -->
-	<slot />
+	{#if $user}
+		<Navbar />
+		<!-- slot is sveltes version of a child -->
+		<slot />
+	{:else}
+		<Auth />
+	{/if}
 </div>
